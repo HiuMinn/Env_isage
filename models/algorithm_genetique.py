307b49ebda38e6bedfc0324d2cddb7c2e25bcacd
hmdang <hieu-minh.dang@insa-lyon.fr>
2025-03-10 15:50:22 +0100

    renomer fichier interface_graphique

--- models/algorithm_genetique.py
+++ models/algorithm_genetique.py
@@ -2,48 +2,66 @@ import numpy as np
 import random
 
 class GA():
-    def __init__(self,nb_individu,nb_generations):
+    def __init__(self,x,nb_individu=100):
         """
         Initialiser les parametres de la classe GA
-        Args:
-            nb_individu (int): nombre d'individu
-            nb_generations (int): nombre de generations pour l'evolution genetique
+        Arg:
+            x (list des arrays): l'ensemble des vecteurs de photos
+            nb_individu (int): le nombre d'individu dans une population
         """
         #initialiser les parametres des
+        self.input = x
         self.nb_individu=nb_individu
-        self.nb_generations=nb_generations
         #parametres de populations
         self.fitness = [] #TODO : definition de fitness
         self.population = []
+        self.init_population()
 
-    def add_individual(self,individual):
+    def init_population(self):
         """
-        Ajouter un individu a la population
-        param
-            individual (object):
-        return:
+        initialiser la première population
+        :return: None
         """
-        #TODO : definir individual (images,veteurs)
-        self.population.append(individual)
-        self.fitness.append(individual.fitness)
+        for i in range(self.nb_individu):
+            self.population.append([]) #TODO: Définir l'individu
+        return
 
-    def selection(self):
+    def tournament_selection(self,tournament_size):
         """
-        selectionner une sous-population a reproduire pour la prochaine generation
+        selectionner une sous-population a reproduire pour la prochaine generation selon tournament
+        argument:
+            tournament_size (int): nombre d'individu a utiliser pour le tournament
         return:
             selected (list): les individus a reproduire
         """
         #TODO
         selected = []
+        for _ in range(len(self.population)):
+            tournament = random.sample(list(zip(self.population,self.fitness)), tournament_size)
+            winner = max(tournament, key=lambda x: x[1])
+            selected.append(winner)
         return selected
 
     def crossover(self, parent1, parent2):
         """
         Appliquer le technique de croisement pour generer de nouvelles descendant
-        param
-            parent1 (object):
-            parent2 (object):
+        Args:
+            parent1, parent2 (object):
         return:
         """
         #TODO
+        return
+
+    def genetic_algorithm(self,nb_generations):
+        """
+        Générer les nouvelles populations
+        Args:
+            nb_generations (int): nombre de generations pour l'évolution genetique
+
+        return: les images les plus pertinentes (meilleurs fitness)
+        """
+        res = []
+        for i in range(nb_generations):
+            sous_population = self.tournament_selection(5)
+
         return
