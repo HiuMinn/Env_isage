d355e5861e36595b3522de6b1debe5e74b4e3885
lilian <lilian.guitart-arnau@insa-lyon.fr>
2025-03-17 09:16:30 +0100

    mise à jour des fichiers de setup

--- mac_initialisation.zsh
+++ mac_initialisation.zsh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Créer l’environnement conda
 conda env create -f environment.yml
-
-# Activer l’environnement
 conda activate env_isage
-
-# Afficher la liste des paquets installés
 conda list
