1cb9c5f2c1f88fb7d85a0c7bd1811937a7c34cf4
lilian <lilian.guitart-arnau@insa-lyon.fr>
2025-04-02 15:07:21 +0200

    réécriture de generate et combine

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ import interface_graphique as ig
 import torch
 import autoencodeur as ae
 
-def get_one_img()
+def get_one_img():
+    pass
 
 def clear_tmp():
     pass
 
 def add_to_tmp(img):
-    pass
+
+    return name
 
 
 def clear_tmp():
@@ -24,39 +26,35 @@ def clear_tmp():
 def add_to_dict(key, value):
     pass
 
-
 def replace_20_first_img_in_directory(nb=20):
     """
     mets 20 img avec les bons noms : les indexes 1,2,...,21 dans le dossier
     """
     clear_tmp()
-    for i in range(nb):
+    for _ in range(nb):
         img = get_one_img()
-        add_to_tmp(img)
-        add_dict
-        
-    return dict_encoded
+        i = add_to_tmp(img)
+        add_to_dict(i,encode(img))
 
 
 def generate_img(l_of_one_img_name, var):
-    """
-    input : liste d'un seul nom d'image
-    rajoute les 4 images générées au dossier
-    """
-    l_vectors = ag.bruitage([dict_encoded[l_of_one_img_name[0]]], sigma = var)
-    for vec in l_vectors:
+    l_vec_son = ag.bruitage([dict_encoded[l_of_one_img_name[0]]], sigma = var) # remplacé par main_mutation si besoin
+    for vec in l_vec_son:
         img = ae.decode(vec)
-        add_to_tmp(img)
+        i = add_to_tmp(img)
+        add_to_dict(i,vec)
 
-    return dict_encoded
 
 
-def combine_img(l_of_img_names):
-    """
-    input : liste de plusieurs noms d'images
-    rajoute les 4 images générées au dossier
-    """
-    return dict_encoded
+def combine_img(l_of_img_names,var):
+    img_vec_dict = read_dict()
+    l_vec_parents = [img_vec_dict[name] for name in l_of_img_names]
+    l_vec_son = ag.fusion_poids_lognormal(l_vec_parents, var)
+
+    for vec in l_vec_son:
+        img = ae.decode(vec)
+        i = add_to_tmp(img)
+        add_to_dict(i,vec)
 
 
 if __name__=='__main__':
